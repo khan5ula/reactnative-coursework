5d93c5f5e7ce3b7f05a4e08c84f3d8a4cdf21ecb
Kristian Hannula <kristian.hannula@proton.me>
2023-08-17 11:34:47 +0300

    graphql has connection error

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,5 +1,4 @@
 import { ApolloProvider } from '@apollo/client'
-import { StatusBar } from 'expo-status-bar'
 import { NativeRouter } from 'react-router-native'
 import Main from './src/components/Main'
 import createApolloClient from './src/utils/apolloClient'
@@ -8,14 +7,11 @@ const apolloClient = createApolloClient()
 
 const App = () => {
   return (
-    <>
-      <NativeRouter>
-        <ApolloProvider client={apolloClient}>
-          <Main />
-        </ApolloProvider>
-      </NativeRouter>
-      <StatusBar style="auto" />
-    </>
+    <NativeRouter>
+      <ApolloProvider client={apolloClient}>
+        <Main />
+      </ApolloProvider>
+    </NativeRouter>
   )
 }
 
--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -1,31 +1,23 @@
-import { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client'
+import { GET_REPOSITORIES } from '../graphql/queries'
+import { useEffect } from 'react'
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState()
-  const [loading, setLoading] = useState(false)
-
-  const fetchRepositories = async () => {
-    setLoading(true)
-
-    try {
-      const response = await fetch('http://192.168.1.101:5000/api/repositories')
-      if (!response.ok) {
-        throw new Error('Network response was not ok')
-      }
-      const json = await response.json()
-      setRepositories(json)
-    } catch (error) {
-      console.error('Error fetching repositories:', error)
-    } finally {
-      setLoading(false)
-    }
-  }
+  const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  })
 
   useEffect(() => {
-    fetchRepositories()
+    console.log(`data: ${JSON.stringify(data)}`)
+    console.log(`error: ${JSON.stringify(error)}`)
   }, [])
 
-  return { repositories, loading, refetch: fetchRepositories }
+  return {
+    repositories: data?.repositories,
+    loading,
+    error,
+    refetch,
+  }
 }
 
 export default useRepositories
